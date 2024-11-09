f79d4302bc43f4f9b247a7533fdc6ab57916163e
isaced <isaced@163.com>
2024-11-09 11:31:45 +0800

    feat: enhance backup script to clear old backup files

--- src/backup.sh
+++ src/backup.sh
@@ -4,16 +4,24 @@ set -eu
 set -o pipefail
 
 # Environment variables
-CURRENT_DATETIME=$(date +%Y%m%d%H%M%S)                   # Backup datetime
-BACKUP_FILE="/backups/backup_${CURRENT_DATETIME}.sql.gz" # Backup file
+CURRENT_DATETIME=$(date +%Y%m%d%H%M%S)                        # Backup datetime
+BACKUP_DIR="/backups"                                         # Backup directory
+BACKUP_FILE="${BACKUP_DIR}/backup_${CURRENT_DATETIME}.sql.gz" # Backup file
 
 # Set Postgres password
 export PGPASSWORD=${POSTGRES_PASSWORD}
 
-echo "Creating backup of $POSTGRES_DATABASE database..."
-
 # Create backup directory
 mkdir -p /backups
+
+# clean up old backups (keep only 10)
+backups_file_count=$(ls ${BACKUP_DIR} -1 | wc -l)
+if [ $backups_file_count -gt 10 ]; then
+  rm ${BACKUP_DIR}/*.sql.gz
+  echo "Old backup files have been deleted!"
+fi
+
+echo "Creating backup of $POSTGRES_DATABASE database..."
 
 # Create backup
 pg_dump --format=custom \
